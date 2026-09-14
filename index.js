63c1a5d82d91b5f9c9069ac279e1a4025df442ce
Parkseojin08 <a01075011775@gmail.com>
2026-09-14 13:38:52 +0900

    test2

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const cors = require('cors');
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send("just testing")
+    res.send("just testing");
 });
+app.get('/test', (req, res) => {
+    res.send("test route");
+})
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('server in running');
